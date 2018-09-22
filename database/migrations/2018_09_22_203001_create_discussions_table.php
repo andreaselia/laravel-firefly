33d54d3b659d80e15273c5a21f9d50930860cade
tomirons <tom.irons@hotmail.com>
2018-09-22 16:45:42 -0400

    create migrations

---
+++ database/migrations/2018_09_22_203001_create_discussions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscussionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discussions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->timestamp('locked_at')->nullable();
+            $table->timestamp('stickied_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discussions');
+    }
+}
