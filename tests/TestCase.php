37df281627357afd61fc124a4f9f6291aeb8376e
tomirons <tom.irons@hotmail.com>
2018-09-22 18:27:29 -0400

    add more setup steps

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,20 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 class TestCase extends OrchestraTestCase
 {
     /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->loadLaravelMigrations();
+
+        $this->artisan('migrate')->run();
+    }
+
+    /**
      * Get package providers.
      *
      * @param  \Illuminate\Foundation\Application  $app
