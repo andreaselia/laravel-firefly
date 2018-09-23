81543e69b7645df5d085c476067e5b334f2b60af
tomirons <tom.irons@hotmail.com>
2018-09-23 17:37:57 -0400

    create forum and tag controllers

---
+++ src/Http/Controllers/ForumController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Firefly\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ForumController extends Controller
+{
+    /**
+     * Show the forum index.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        //
+    }
+}
---
+++ src/Http/Controllers/TagController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Firefly\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class TagController extends Controller
+{
+    /**
+     * Show the discussions for the specified tag.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show()
+    {
+        //
+    }
+}
