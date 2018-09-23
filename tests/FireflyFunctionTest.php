5c5bacdb6a7f1739c5f359a917f4132eaef636c8
Andreas Elia <andreaselia@live.co.uk>
2018-09-23 09:36:18 +0000

    Apply fixes from StyleCI

--- tests/FireflyFunctionTest.php
+++ tests/FireflyFunctionTest.php
@@ -7,14 +7,16 @@ class FireflyFunctionTest extends TestCase
     /**
      * @return void
      */
-    public function test_true_is_true() {
+    public function test_true_is_true()
+    {
         $this->assertTrue(true);
     }
 
     /**
      * @return void
      */
-    public function test_false_is_false() {
+    public function test_false_is_false()
+    {
         $this->assertFalse(false);
     }
 }
