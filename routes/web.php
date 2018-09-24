1eb5d5dad615d586850a4b79e21ab97bd3dd6899
tomirons <tom.irons@hotmail.com>
2018-09-24 16:39:16 -0400

    add some more routes for the discussions

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,12 @@
 
 Route::get('/', 'ForumController@index');
 
-// Tags...
-Route::get('/{tag}', 'TagController@show');
+// Groups...
+Route::get('{tag}', 'GroupController@show')->name('group.show');
+
+// Discussions...
+Route::get('{id}-{slug}', 'DiscussionController@show')->name('discussion.show');
+Route::get('{tag}/discussion/create', 'DiscussionController@create')->name('discussion.create');
+Route::post('{tag}/discussion', 'DiscussionController@store')->name('discussion.store');
+Route::put('{id}-{slug}', 'DiscussionController@update')->name('discussion.update');
+Route::delete('{id}-{slug}', 'DiscussionController@delete')->name('discussion.delete');
