d1941ccae92874c82d05ae9d43e6a1ae93260a1d
tomirons <tom.irons@hotmail.com>
2018-09-25 23:53:22 -0400

    rename controller

---
+++ src/Http/Controllers/GroupController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Firefly\Http\Controllers;
+
+class GroupController extends Controller
+{
+    /**
+     * Show the discussions for the specified tag.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show()
+    {
+        //
+    }
+}
