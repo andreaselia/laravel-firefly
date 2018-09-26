e8d51aafa2e20737fb71a2c50a1799f539bff1fe
tomirons <tom.irons@hotmail.com>
2018-09-25 23:46:18 -0400

    update url parameter

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', 'ForumController@index');
 Route::get('{tag}', 'GroupController@show')->name('group.show');
 
 // Discussions...
-Route::get('{id}-{slug}', 'DiscussionController@show')->name('discussion.show');
+Route::get('{discussion}-{slug}', 'DiscussionController@show')->name('discussion.show');
 Route::get('{tag}/discussion/create', 'DiscussionController@create')->name('discussion.create');
 Route::post('{tag}/discussion', 'DiscussionController@store')->name('discussion.store');
-Route::put('{id}-{slug}', 'DiscussionController@update')->name('discussion.update');
-Route::delete('{id}-{slug}', 'DiscussionController@delete')->name('discussion.delete');
+Route::put('{discussion}-{slug}', 'DiscussionController@update')->name('discussion.update');
+Route::delete('{discussion}-{slug}', 'DiscussionController@delete')->name('discussion.delete');
