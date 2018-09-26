f7214a9d1024e1eae3074bc874502235925fc263
tomirons <tom.irons@hotmail.com>
2018-09-25 22:52:33 -0400

    update fillable and add sluggable

--- src/Discussion.php
+++ src/Discussion.php
@@ -3,17 +3,33 @@
 namespace Firefly;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Discussion extends Model
 {
+    use HasSlug;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'title', 'slug'
+        'title'
     ];
+
+    /**
+     * Get the options for generating the slug.
+     *
+     * @return \Spatie\Sluggable\SlugOptions
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 
     /**
      * Get all the posted for the discussion.
--- src/Group.php
+++ src/Group.php
@@ -3,15 +3,31 @@
 namespace Firefly;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Group extends Model
 {
+    use HasSlug;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'slug', 'color',
+        'name', 'color',
     ];
+
+    /**
+     * Get the options for generating the slug.
+     *
+     * @return \Spatie\Sluggable\SlugOptions
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
