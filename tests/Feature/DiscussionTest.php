f3ff445ec0016ac883ebb09666236c56393391e1
tomirons <tom.irons@hotmail.com>
2018-09-26 14:09:35 -0400

    add more asserts and tests

--- tests/Feature/DiscussionTest.php
+++ tests/Feature/DiscussionTest.php
@@ -11,18 +11,22 @@ class DiscussionTest extends TestCase
     public function test_discussion_was_created()
     {
         $crawler = $this->actingAs($this->getUser())
-            ->postJson('forum/example-tag/discussion', [
+            ->post('forum/example-tag/discussion', [
                 'title' => 'Foo Bar',
             ]);
 
         $discussions = Discussion::all();
 
         $this->assertTrue($discussions->count() == 2);
-
         $this->assertDatabaseHas('discussions', [
             'title' => 'Foo Bar',
             'slug' => 'foo-bar',
         ]);
+
+        $discussion = Discussion::find(2);
+
+        $crawler->assertStatus(302);
+        $crawler->assertRedirect('forum/' . $discussion->uri);
     }
 
     public function test_discussion_was_updated()
@@ -30,14 +34,62 @@ class DiscussionTest extends TestCase
         $discussion = $this->getDiscussion();
 
         $crawler = $this->actingAs($this->getUser())
-            ->putJson('forum/' . $discussion->uri, [
+            ->put('forum/' . $discussion->uri, [
                 'title' => 'Bar Foo',
             ]);
 
         $discussion = $this->getDiscussion()->refresh();
 
         $this->assertTrue($discussion->title == 'Bar Foo');
-
         $this->assertTrue($discussion->slug == 'bar-foo');
+
+        $crawler->assertStatus(302);
+        $crawler->assertRedirect('forum/' . $discussion->uri);
+    }
+
+    public function test_discussion_was_soft_deleted()
+    {
+        $discussion = $this->getDiscussion();
+
+        $crawler = $this->actingAs($this->getUser())
+            ->delete('forum/' . $discussion->uri);
+
+        $discussion = $this->getDiscussion()->refresh();
+
+        $this->assertFalse($discussion->exists());
+        $this->assertNotNull($discussion->deleted_at);
+
+        $crawler->assertStatus(302);
+        $crawler->assertRedirect('/forum');
+    }
+
+    public function test_discussion_gets_locked()
+    {
+        $discussion = $this->getDiscussion();
+
+        $crawler = $this->actingAs($this->getUser())
+            ->put('forum/' . $discussion->uri . '/lock');
+
+        $discussion = $this->getDiscussion()->refresh();
+
+        $this->assertNotNull($discussion->locked_at);
+
+        $crawler->assertStatus(302);
+        $crawler->assertRedirect('/forum/' . $discussion->uri);
+    }
+
+    public function test_discussion_gets_unlocked()
+    {
+        $discussion = $this->getDiscussion()->lock();
+
+        $crawler = $this->actingAs($this->getUser())
+            ->put('forum/' . $discussion->uri . '/unlock');
+
+        $discussion = $this->getDiscussion()->refresh();
+
+        $this->assertNull($discussion->locked_at);
+
+        $crawler->assertStatus(302);
+        $crawler->assertRedirect('/forum/' . $discussion->uri);
     }
 }
