0b7f2eccf84c1e12f76df4e14ce4c9f3e1bb1ea0
tomirons <tom.irons@hotmail.com>
2018-10-04 11:32:07 -0400

    combine create/update validation tests

--- tests/Feature/DiscussionTest.php
+++ tests/Feature/DiscussionTest.php
@@ -33,17 +33,6 @@ class DiscussionTest extends TestCase
         $crawler->assertLocation('forum/' . $discussion->uri);
     }
 
-    public function test_creation_validation_failed()
-    {
-        $crawler = $this->actingAs($this->getUser())
-            ->postJson('forum/example-tag/discussion', [
-                'title' => 'Foo',
-            ]);
-
-        $crawler->assertStatus(422);
-        $crawler->assertJsonValidationErrors('title');
-    }
-
     public function test_discussion_was_updated()
     {
         $discussion = $this->getDiscussion();
@@ -60,19 +49,6 @@ class DiscussionTest extends TestCase
 
         $crawler->assertRedirect();
         $crawler->assertLocation('forum/' . $discussion->uri);
-    }
-
-    public function test_update_validation_failed()
-    {
-        $discussion = $this->getDiscussion();
-
-        $crawler = $this->actingAs($this->getUser())
-            ->putJson('forum/' . $discussion->uri, [
-                'title' => 'Foo',
-            ]);
-
-        $crawler->assertStatus(422);
-        $crawler->assertJsonValidationErrors('title');
     }
 
     public function test_discussion_was_soft_deleted()
@@ -181,5 +157,115 @@ class DiscussionTest extends TestCase
 
         $this->assertNull($discussion->hidden_at);
         $this->assertNotInstanceOf(Carbon::class, $discussion->hidden_at);
+    }
+
+    public function test_title_is_required()
+    {
+        $title = '';
+        // Create
+        $crawler = $this->actingAs($this->getUser())
+            ->postJson('forum/example-tag/discussion', [
+                'title' => $title,
+            ]);
+
+        $crawler->assertStatus(422);
+        $crawler->assertJson([
+            'errors' => [
+                'title' => [
+                    'The title field is required.'
+                ]
+            ]
+        ]);
+
+        // Update
+        $discussion = $this->getDiscussion();
+
+        $crawler = $this->actingAs($this->getUser())
+            ->putJson('forum/' . $discussion->uri, [
+                'title' => $title,
+            ]);
+
+        $crawler->assertStatus(422);
+        $crawler->assertJson([
+            'errors' => [
+                'title' => [
+                    'The title field is required.'
+                ]
+            ]
+        ]);
+    }
+
+    public function test_title_has_at_least_5_characters()
+    {
+        $title = 'Foo';
+
+        // Create
+        $crawler = $this->actingAs($this->getUser())
+            ->postJson('forum/example-tag/discussion', [
+                'title' => $title,
+            ]);
+
+        $crawler->assertStatus(422);
+        $crawler->assertJson([
+            'errors' => [
+                'title' => [
+                    'The title must be at least 5 characters.'
+                ]
+            ]
+        ]);
+
+        // Update
+        $discussion = $this->getDiscussion();
+
+        $crawler = $this->actingAs($this->getUser())
+            ->putJson('forum/' . $discussion->uri, [
+                'title' => $title,
+            ]);
+
+        $crawler->assertStatus(422);
+        $crawler->assertJson([
+            'errors' => [
+                'title' => [
+                    'The title must be at least 5 characters.'
+                ]
+            ]
+        ]);
+    }
+
+    public function test_title_has_a_max_of_255_characters()
+    {
+        $title = str_random(256);
+
+        // Create
+        $crawler = $this->actingAs($this->getUser())
+            ->postJson('forum/example-tag/discussion', [
+                'title' => $title,
+            ]);
+
+        $crawler->assertStatus(422);
+        $crawler->assertJson([
+            'errors' => [
+                'title' => [
+                    'The title may not be greater than 255 characters.'
+                ]
+            ]
+        ]);
+
+        // Update
+        $discussion = $this->getDiscussion();
+
+        $crawler = $this->actingAs($this->getUser())
+            ->putJson('forum/' . $discussion->uri, [
+                'title' => $title,
+            ]);
+
+        $crawler->assertStatus(422);
+        $crawler->assertJson([
+            'errors' => [
+                'title' => [
+                    'The title may not be greater than 255 characters.'
+                ]
+            ]
+        ]);
     }
 }
