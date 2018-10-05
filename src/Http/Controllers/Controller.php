6ef005be5674a5f2f93ddbb013479f67c40607e0
tomirons <tom.irons@hotmail.com>
2018-10-05 12:06:43 -0400

    add trait to base controller

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,9 +2,10 @@
 
 namespace Firefly\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests;
 }
