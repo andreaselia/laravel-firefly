2bd212d105aabc263be9dcef130ce57c0758fe1b
Andreas Elia <andreaselia@live.co.uk>
2018-10-06 21:47:15 +0100

    frontend fixes and groups page

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -5,8 +5,10 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Groups</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis quos aut sequi totam ducimus nihil, vitae repellendus expedita quas nemo.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <h1>Groups</h1>
+                    <a href="#" class="btn btn-green">Add Group</a>
+                </div>
             </div>
         </div>
     </div>
