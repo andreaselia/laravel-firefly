8a7f4f21d7dbc2ba6d091813db3c8c1adb21f7af
Andreas Elia <andreaselia@live.co.uk>
2018-10-09 22:33:02 +0100

    remove list

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -5,22 +5,24 @@
     <div class="row justify-content-center">
         <div class="col-8">
             @foreach ($posts as $post)
-                <li class="list-group-item list-group-item-action">
-                    <p>{{ __('Posted by') }} {{ $post->user->name }}, {{ $post->created_at->diffForHumans() }}</p>
+                <div class="list-group-item list-group-item-action">
+                    <p>{{ $post->user->name }} {{ $post->created_at->diffForHumans() }}</p>
 
-                    <p class="mb-0">{{ $post->content }}</p>
-                </li>
+                    <p>{{ $post->content }}</p>
+                </div>
             @endforeach
 
             {{-- TODO: move to modal or keep inline? --}}
-            <form action="{{ route('post.store', [$discussion->id, $discussion->slug]) }}" method="POST">
-                <div class="form-group">
-                    <label for="content">{{ __('Content') }}</label>
-                    <textarea name="content" id="content" class="form-control" rows="3">{{ old('content') }}</textarea>
-                </div>
+            <div class="mt-5">
+                <form action="{{ route('post.store', [$discussion->id, $discussion->slug]) }}" method="POST">
+                    <div class="form-group">
+                        <label for="content">{{ __('Content') }}</label>
+                        <textarea name="content" id="content" class="form-control" rows="3">{{ old('content') }}</textarea>
+                    </div>
 
-                <button type="submit" class="btn btn-blue">{{ __('Submit Reply') }}</button>
-            </form>
+                    <button type="submit" class="btn btn-blue">{{ __('Submit Reply') }}</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
