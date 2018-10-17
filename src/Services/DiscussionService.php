15c6f80dc861c87fde8c06ec5298762e9b011b1e
tomirons <tom.irons@hotmail.com>
2018-10-17 12:20:48 -0400

    update the make method

--- src/Services/DiscussionService.php
+++ src/Services/DiscussionService.php
@@ -2,22 +2,36 @@
 
 namespace Firefly\Services;
 
+use Firefly\Group;
 use Illuminate\Http\Request;
 
 class DiscussionService
 {
     /**
-     * Make a new Discussion instance and attach it to the user.
+     * Make a new discussion instance.
      *
      * @param \Illuminate\Http\Request $request
+     * @param \Firefly\Group $group
      * @return mixed
      */
-    public function make(Request $request)
+    public function make(Request $request, Group $group)
     {
-        $discussion = $request->user()->discussions()->make(
+        $user = $request->user();
+
+        // Make the discussion and attach it to the user
+        $discussion = $user->discussions()->make(
             $request->only('title')
         );
 
-        return $discussion;
+        $group->discussions()->save($discussion);
+
+        // Make the post and attach it to the user
+        $post = $user->posts()->make(
+            $request->only('content')
+        );
+
+        $discussion->posts()->save($post);
+
+        return $discussion->refresh();
     }
 }
