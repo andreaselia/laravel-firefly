ec4d9ecbd2e0e22f662fc3868a0948fb35cc3127
Andreas Elia <andreaselia@live.co.uk>
2018-10-27 12:37:36 +0100

    new variables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,40 +19,37 @@
 <body>
     <div id="app">
         <nav class="navbar">
-            <div class="container">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle stroke="#121212" stroke-width="3" cx="16" cy="16" r="14.5"/><path d="M14.554 11.033l6.79 4.514a1 1 0 0 1 .002 1.664l-6.79 4.541A1 1 0 0 1 13 20.921v-9.055a1 1 0 0 1 1.554-.833z" fill="#121212"/></g></svg>
-
-                    {{ config('app.name', 'Firefly') }}
                 </a>
 
                 <div class="navbar-items">
                     <ul>
-                        <li class="nav-item">
-                            <a href="{{ route('firefly.group.index') }}" class="nav-link">{{ __('Groups') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('firefly.forum.index') }}" class="nav-link">{{ __('Discussions') }}</a>
-                        </li>
-                    </ul>
-
-                    <ul>
                         @guest
+                            <li class="nav-item">
+                                <a href="{{ route('firefly.group.index') }}" class="nav-link">{{ __('Groups') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('firefly.forum.index') }}" class="nav-link">{{ __('Discussions') }}</a>
+                            </li>
+                        @else
                             <li class="nav-item">
                                 <a href="{{ route('login') }}" class="nav-link">{{ __('Login') }}</a>
                             </li>
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="nav-link">{{ __('Register') }}</a>
+                            </li>
+                        @endguest
+                    </ul>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a href="{{ route('register') }}" class="nav-link btn btn-blue">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
+                    <ul>
+                        @auth
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }}
+                                    Hello, {{ Auth::user()->name }}
                                 </a>
-                                
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
@@ -63,7 +60,7 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        @endauth
                     </ul>
                 </div>
             </div>
@@ -71,7 +68,7 @@
 
         @yield('hero')
 
-        <main>
+        <main class="py-4">
             @yield('content')
         </main>
 
