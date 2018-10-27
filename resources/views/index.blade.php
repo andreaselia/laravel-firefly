0b3dbe58ebd306618288e83f4b734e95250d64ee
Andreas Elia <andreaselia@live.co.uk>
2018-10-27 19:30:05 +0100

    discussion list changes, title typo, styling to variable changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-between align-items-center mb-4">
-        <h1 class="mb-0">{{ __('Groups') }}</h1>
+        <h1 class="mb-0">{{ __('Discussions') }}</h1>
 
         @if (Auth::check() && Auth::user()->can('create', Firefly\Discussion::class))
             <button type="button" class="btn btn-primary" @click.prevent="toggleModal('newGroup')">
@@ -15,23 +15,33 @@
     <div class="card">
         <div class="card-body">
             @foreach ($discussions as $discussion)
-                <div class="discussion">
-                    <h3>
-                        <a href="{{ route('firefly.discussion.show', [$discussion->id, $discussion->slug]) }}">
-                            {{ $discussion->title }}
-                        </a>
-                    </h3>
+                <div class="discussion-item d-flex justify-content-between align-items-center">
+                    <div>
+                        <h3 class="mb-0">
+                            <a href="{{ route('firefly.discussion.show', [$discussion->id, $discussion->slug]) }}">
+                                {{ $discussion->title }}
+                            </a>
+                        </h3>
 
-                    <div class="list-item-meta">
-                        {{ __('Posted by') }} {{ $discussion->user->name }}
-                        <span class="mx-2">{{ $discussion->created_at->diffForHumans() }}</span>
-                        3 {{ __('replies') }}
+                        <div class="discussion-item-meta">
+                            {{ $discussion->user->name }}
+                            <span class="mx-2">{{ $discussion->created_at->diffForHumans() }}</span>
+                            {{ count($discussion->posts) }} {{ __('replies') }}
+                        </div>
+                    </div>
+
+                    <div class="d-flex">
+                        @foreach ($discussion->groups as $group)
+                            <div class="group-item rounded-circle mb-0 ml-2" style="background: {{ $group->color }};"></div>
+                        @endforeach
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
 
-    {{-- @include('firefly::partials.discussion-list') --}}
+    <div class="mt-3">
+        {!! $discussions->links() !!}
+    </div>
 </div>
 @endsection
