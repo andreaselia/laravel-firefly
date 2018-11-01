82661d50fe7d982ced6ef6f9d9e63862b75ded29
tomirons <tom.irons@hotmail.com>
2018-11-01 12:40:09 -0400

    remove/update tests

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -45,10 +45,8 @@ class PostTest extends TestCase
 
         $this->assertEquals('Bar Foo', $post->content);
 
-        $crawler->assertOk();
-        $crawler->assertJson([
-            'content' => 'Bar Foo',
-        ]);
+        $crawler->assertRedirect();
+        $crawler->assertLocation('forum/' . $post->discussion->uri);
     }
 
     public function test_post_was_soft_deleted()
@@ -65,38 +63,6 @@ class PostTest extends TestCase
         $this->assertNotNull($post->deleted_at);
 
         $crawler->assertOk();
-    }
-
-    public function test_post_was_hidden_successfully()
-    {
-        $post = $this->getPost();
-
-        $crawler = $this->actingAs($this->getUser())
-            ->patchJson('forum/posts/' . $post->id . '/hide');
-
-        $post->refresh();
-
-        $crawler->assertRedirect();
-        $crawler->assertLocation('forum/' . $post->discussion->uri);
-        
-        $this->assertNotNull($post->hidden_at);
-        $this->assertInstanceOf(Carbon::class, $post->hidden_at);
-    }
-
-    public function test_post_was_unhidden_successfully()
-    {
-        $post = $this->getPost()->hide();
-
-        $crawler = $this->actingAs($this->getUser())
-            ->patchJson('forum/posts/' . $post->id . '/unhide');
-        
-        $post->refresh();
-
-        $crawler->assertRedirect();
-        $crawler->assertLocation('forum/' . $post->discussion->uri);
-
-        $this->assertNull($post->hidden_at);
-        $this->assertNotInstanceOf(Carbon::class, $post->hidden_at);
     }
 
     public function test_content_is_required()
