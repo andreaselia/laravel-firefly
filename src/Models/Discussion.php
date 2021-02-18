58dd1ac6a93a0a8b9a626d3d6976609887ca5542
Andreas Elia <AndreasElia@users.noreply.github.com>
2021-02-18 23:59:44 +0000

    Apply fixes from StyleCI

--- src/Models/Discussion.php
+++ src/Models/Discussion.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Str;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
