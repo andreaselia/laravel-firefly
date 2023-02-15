3c3ed69a71bcdc6e127f1e5b56da348de3c2d1d8
Andreas Elia <andreaselia@live.co.uk>
2023-02-15 23:42:57 +0000

    attempt to update the code to make it more readable/maintainable

--- src/Traits/SanitizesPosts.php
+++ src/Traits/SanitizesPosts.php
@@ -6,37 +6,53 @@ use Firefly\Features;
 
 trait SanitizesPosts
 {
-    /**
-     * Returns sanitized content for Posts when using rich formatting and the wysiwyg editor.
-     *
-     * @param  array  $requestData
-     * @return array
-     */
-    public function getSanitizedPostData(array $requestData)
+    public function getSanitizedPostData(array $requestData): array
     {
-        $formatting = (isset($requestData['formatting'])) ? $requestData['formatting'] : 'plain';
+        $formatting = $requestData['formatting'] ?? 'plain';
 
         if (Features::enabled('wysiwyg') && $formatting === 'rich') {
-            $notAllowedTags = ['script'];
-            foreach ($notAllowedTags as $tag) {
-                $requestData['content'] = preg_replace('/<\\/?'.$tag.'(.|\\s)*?>/i', '', $requestData['content']);
-            }
+            $requestData['content'] = $this->sanitizeContent($requestData['content']);
 
-            $prevUseErrors = libxml_use_internal_errors(true);
-            $dom = new \DOMDocument();
-            $requestData['content'] = mb_convert_encoding($requestData['content'], 'HTML-ENTITIES', 'UTF-8');
-            $dom->loadHTML('<root>'.$requestData['content'].'</root>', LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-            $xpath = new \DOMXPath($dom);
-            $selfClosingElements = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
-            foreach ($xpath->query('//*[not(node())]') as $node) {
-                if (! in_array($node->localName, $selfClosingElements)) {
-                    $node->parentNode->removeChild($node);
-                }
-            }
-            $requestData['content'] = substr($dom->saveHTML(), 6, -8);
-            libxml_use_internal_errors($prevUseErrors);
+            $requestData['content'] = $this->convertToHtmlEntities($requestData['content']);
+
+            $requestData['content'] = $this->removeEmptyTags($requestData['content']);
+
+            $requestData['content'] = $this->removeRootTag($requestData['content']);
         }
 
         return $requestData;
+    }
+
+    private function sanitizeContent(string $content): string
+    {
+        $notAllowedTags = ['script'];
+        foreach ($notAllowedTags as $tag) {
+            $content = preg_replace('/<\\/?'.$tag.'(.|\\s)*?>/i', '', $content);
+        }
+        return $content;
+    }
+
+    private function convertToHtmlEntities(string $content): string
+    {
+        return mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8');
+    }
+
+    private function removeEmptyTags(string $content): string
+    {
+        $dom = new \DOMDocument();
+        $dom->loadHTML($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $xpath = new \DOMXPath($dom);
+        $selfClosingElements = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
+        foreach ($xpath->query('//*[not(node())]') as $node) {
+            if (! in_array($node->localName, $selfClosingElements)) {
+                $node->parentNode->removeChild($node);
+            }
+        }
+        return substr($dom->saveHTML(), 6, -8);
+    }
+
+    private function removeRootTag(string $content): string
+    {
+        return str_replace(['<root>', '</root>'], '', $content);
     }
 }
