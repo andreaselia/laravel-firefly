644f94121eecc0e7f65466ce45a4fe00d43141dc
AndreasElia <andreaselia@live.co.uk>
2023-02-16 00:43:36 +0000

    styleci fix

--- src/Services/DiscussionService.php
+++ src/Services/DiscussionService.php
@@ -33,7 +33,7 @@ class DiscussionService
         $group->discussions()->save($discussion);
 
         $data = $this->getSanitizedPostData($request->only(['formatting', 'content']));
-        
+
         // Set the post as the initial post of the discussion
         $data['is_initial_post'] = 1;
 
